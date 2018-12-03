c9140cfdaca43b6b8757c9eaf6161fb967f79916
dark705 <dark705r@mail.ru>
2018-12-03 13:30:38 +0300

    Add xorEncoder function

--- functions.php
+++ functions.php
@@ -3,6 +3,12 @@ function xorEncoder($str, $key = null){
     if (is_null($key)) {
         return $str;
     } else {
-
+        $l = strlen($str);
+        $k = strlen($key);
+        $r = '';
+        for($i = 0; $i < $l; $i++){
+            $r .= $str[$i] ^ $key[$i % $k];
+        }
+        return $r;
     }
 }
